45f17e78980b83f39a8b57bcdd849884d6d5cbbc
mhd-zaid <zaidmouhamad@gmail.com>
2023-07-25 18:18:23 +0200

    feat(user): update password

---
+++ src/EventSubscriber/EncodePasswordSubscriber.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class EncodePasswordSubscriber implements EventSubscriberInterface
+{
+    public function __construct(private UserPasswordHasherInterface $passwordHasher){}
+
+    public function getSubscribedEvents(): array
+    {
+        return [
+            Events::prePersist,
+            Events::preUpdate,
+        ];
+    }
+
+    public function prePersist(LifecycleEventArgs $args): void
+    {
+        $object = $args->getObject();
+
+        
+        if ($object instanceof User) {
+            $this->updatePassword($object);
+        }
+    }
+
+    public function preUpdate(LifecycleEventArgs $args): void
+    {
+        $object = $args->getObject();
+
+        if ($object instanceof User) {
+            $this->updatePassword($object);
+        }
+    }
+
+    private function updatePassword(User $object): void
+    {
+        if ($object->getPlainPassword()) {
+            $object->setPassword($this->passwordHasher->hashPassword($object, $object->getPlainPassword()));
+        }
+    }
+}
